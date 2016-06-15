43532a283fafbfc40a7a2b3363d4bec13c01f2e8
Joeri Samson <joerisamson@gmail.com>
2016-06-15 13:35:33 +0200

    Prevent editing of Qualified/Favourite after certain time
    That time is the start of the second wave of matches

--- phoenix/web/controllers/favourite_controller.ex
+++ phoenix/web/controllers/favourite_controller.ex
@@ -12,41 +12,47 @@ defmodule EcPredictions.FavouriteController do
 
   def update(conn, %{"favourites" => favourites_params} = params) do
     user = conn |> Guardian.Plug.current_resource() |> Repo.preload(:favourites)
-
-    changes = Enum.flat_map favourites_params, fn {country, favourite} ->
-      country_id = ~r/country\[(\d+)\]/ |> Regex.replace(country, "\\1") |> String.to_integer
-      previously = Enum.find user.favourites, fn
-        %Favourite{country_id: ^country_id} -> true
-        _ -> false
-      end
-
-      if previously do
-        [Favourite.changeset(previously, %{delete: favourite == "false"})]
-      else
-        if favourite == "true" do
-          [Favourite.changeset(%Favourite{}, %{country_id: country_id, user_id: user.id})]
-        else
-          []
-        end
-      end
-    end
-
-    favourites = Enum.reject(changes, fn fav -> fav.action == :delete end)
     groups = Group |> Repo.all() |> Repo.preload(country_groups: :country)
 
-    if length(favourites) > 8 do
+    if Timex.DateTime.from_erl({{2016,6,15},{13,0,0}}) < Timex.DateTime.now do
       conn
-      |> put_flash(:error, "Selected too many favourites")
+      |> put_flash(:error, "Too late to modify selection")
       |> render("show.html", user: user, favourites: user.favourites, groups: groups)
     else
-      user
-      |> User.changeset(%{})
-      |> Ecto.Changeset.put_assoc(:favourites, changes)
-      |> Repo.update!()
+      changes = Enum.flat_map favourites_params, fn {country, favourite} ->
+        country_id = ~r/country\[(\d+)\]/ |> Regex.replace(country, "\\1") |> String.to_integer
+        previously = Enum.find user.favourites, fn
+          %Favourite{country_id: ^country_id} -> true
+          _ -> false
+        end
 
-      conn
-      |> put_flash(:info, "Updated selection")
-      |> redirect(to: favourite_path(conn, :show))
+        if previously do
+          [Favourite.changeset(previously, %{delete: favourite == "false"})]
+        else
+          if favourite == "true" do
+            [Favourite.changeset(%Favourite{}, %{country_id: country_id, user_id: user.id})]
+          else
+            []
+          end
+        end
+      end
+
+      favourites = Enum.reject(changes, fn fav -> fav.action == :delete end)
+
+      if length(favourites) > 8 do
+        conn
+        |> put_flash(:error, "Selected too many favourites")
+        |> render("show.html", user: user, favourites: user.favourites, groups: groups)
+      else
+        user
+        |> User.changeset(%{})
+        |> Ecto.Changeset.put_assoc(:favourites, changes)
+        |> Repo.update!()
+
+        conn
+        |> put_flash(:info, "Updated selection")
+        |> redirect(to: favourite_path(conn, :show))
+      end
     end
   end
 end
--- phoenix/web/controllers/qualified_controller.ex
+++ phoenix/web/controllers/qualified_controller.ex
@@ -12,41 +12,47 @@ defmodule EcPredictions.QualifiedController do
 
   def update(conn, %{"qualifieds" => qualifieds_params} = params) do
     user = conn |> Guardian.Plug.current_resource() |> Repo.preload(:qualifieds)
-
-    changes = Enum.flat_map qualifieds_params, fn {country, qualified} ->
-      country_id = ~r/country\[(\d+)\]/ |> Regex.replace(country, "\\1") |> String.to_integer
-      previously = Enum.find user.qualifieds, fn
-        %Qualified{country_id: ^country_id} -> true
-        _ -> false
-      end
-
-      if previously do
-        [Qualified.changeset(previously, %{delete: qualified == "false"})]
-      else
-        if qualified == "true" do
-          [Qualified.changeset(%Qualified{}, %{country_id: country_id, user_id: user.id})]
-        else
-          []
-        end
-      end
-    end
-
-    qualifieds = Enum.reject(changes, fn qual -> qual.action == :delete end)
     groups = Group |> Repo.all() |> Repo.preload(country_groups: :country)
 
-    if length(qualifieds) > 16 do
+    if Timex.DateTime.from_erl({{2016,6,15},{13,0,0}}) < Timex.DateTime.now do
       conn
-      |> put_flash(:error, "Selected too many qualified countries")
-      |> render("show.html", user: user, qualifieds: user.qualifieds, groups: groups)
+      |> put_flash(:error, "Too late to modify selection")
+      |> render("show.html", user: user, favourites: user.favourites, groups: groups)
     else
-      user
-      |> User.changeset(%{})
-      |> Ecto.Changeset.put_assoc(:qualifieds, changes)
-      |> Repo.update!()
+      changes = Enum.flat_map qualifieds_params, fn {country, qualified} ->
+        country_id = ~r/country\[(\d+)\]/ |> Regex.replace(country, "\\1") |> String.to_integer
+        previously = Enum.find user.qualifieds, fn
+          %Qualified{country_id: ^country_id} -> true
+          _ -> false
+        end
 
-      conn
-      |> put_flash(:info, "Updated selection")
-      |> redirect(to: qualified_path(conn, :show))
+        if previously do
+          [Qualified.changeset(previously, %{delete: qualified == "false"})]
+        else
+          if qualified == "true" do
+            [Qualified.changeset(%Qualified{}, %{country_id: country_id, user_id: user.id})]
+          else
+            []
+          end
+        end
+      end
+
+      qualifieds = Enum.reject(changes, fn qual -> qual.action == :delete end)
+
+      if length(qualifieds) > 16 do
+        conn
+        |> put_flash(:error, "Selected too many qualified countries")
+        |> render("show.html", user: user, qualifieds: user.qualifieds, groups: groups)
+      else
+        user
+        |> User.changeset(%{})
+        |> Ecto.Changeset.put_assoc(:qualifieds, changes)
+        |> Repo.update!()
+
+        conn
+        |> put_flash(:info, "Updated selection")
+        |> redirect(to: qualified_path(conn, :show))
+      end
     end
   end
 end
