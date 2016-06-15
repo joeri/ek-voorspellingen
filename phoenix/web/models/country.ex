43217d9df948d31292aa4ddfb86ea70f4d9dadf5
Joeri Samson <joerisamson@gmail.com>
2016-06-16 01:16:10 +0200

    Calculate scores for favourite countries
    Not really tested very well, but should work

--- phoenix/web/models/country.ex
+++ phoenix/web/models/country.ex
@@ -4,7 +4,20 @@ defmodule EcPredictions.Country do
   schema "countries" do
     field :name, :string
 
+    has_many :home_games, EcPredictions.Game, foreign_key: :home_country_id
+    has_many :away_games, EcPredictions.Game, foreign_key: :away_country_id
+
     timestamps
+  end
+
+  def points(home_games, away_games) do
+    home_points = home_games
+    |> Enum.map(fn game -> score_for(game.home_country_goals, game.away_country_goals) end)
+    |> Enum.sum
+    away_points = away_games
+    |> Enum.map(fn game -> score_for(game.away_country_goals, game.home_country_goals) end)
+    |> Enum.sum
+    home_points + away_points
   end
 
   @doc """
@@ -14,5 +27,14 @@ defmodule EcPredictions.Country do
     country
     |> cast(params, [:name])
     |> validate_required([:name])
+  end
+
+  defp score_for(self_goals, other_goals) do
+    cond do
+      self_goals == nil         -> 0
+      self_goals > other_goals  -> 5 + self_goals
+      self_goals == other_goals -> 3 + self_goals
+      self_goals < other_goals  -> 1 + self_goals
+    end
   end
 end
--- phoenix/web/models/score.ex
+++ phoenix/web/models/score.ex
@@ -1,9 +1,6 @@
 defmodule EcPredictions.Score do
   use EcPredictions.Web, :model
-  alias EcPredictions.Repo
-  alias EcPredictions.User
-  alias EcPredictions.Score
-  alias EcPredictions.Prediction
+  alias EcPredictions.{Repo,User,Score,Prediction,Country}
 
   schema "scores" do
     field :points, :integer
@@ -12,16 +9,31 @@ defmodule EcPredictions.Score do
     timestamps
   end
 
-  def calculate(user) do
-    user.predictions
+  def calculate(user, country_scores \\ calculate_all_countries) do
+    predictions = user.predictions
     |> Enum.map(fn p -> Prediction.points(p) end)
     |> Enum.sum
+
+    favourites = user.favourites
+    |> Enum.map(fn f -> country_scores[f.country_id] end)
+    |> Enum.sum
+
+    predictions + favourites
+  end
+
+  def calculate_all_countries do
+    Country
+    |> Repo.all()
+    |> Repo.preload([:home_games, :away_games])
+    |> Enum.reduce(%{}, fn (country, acc) ->
+        Map.put_new(acc, country.id, Country.points(country.home_games, country.away_games))
+    end)
   end
 
   def update_all do
-    inter = User
+    User
     |> Repo.all()
-    |> Repo.preload([:score, predictions: :game])
+    |> Repo.preload([:score, :favourites, predictions: :game])
     |> Enum.map(fn user -> user.score |> Score.changeset(%{points: Score.calculate(user)}) |> Repo.update end)
   end
 
