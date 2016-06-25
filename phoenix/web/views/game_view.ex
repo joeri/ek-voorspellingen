9109027dc4937ad908ae022bdff84f3129eec3da
Joeri Samson <joerisamson@gmail.com>
2016-06-26 01:30:20 +0200

    Show right outcome in game view

--- phoenix/web/views/game_view.ex
+++ phoenix/web/views/game_view.ex
@@ -8,4 +8,16 @@ defmodule EcPredictions.GameView do
     result
   end
 
+  def format_outcome(game) do
+    if @game.round == 1 do
+      "#{@game.home_country_goals}-#{@game.away_country_goals}"
+    else
+      if @game.final_home_country_goals == @game.final_away_country_goals do
+        "#{@game.final_home_country_goals}-#{@game.final_away_country_goals}"
+      else
+        "#{@game.final_home_country_goals}-#{@game.final_away_country_goals} (#{@game.penalties_home_country_goals}-#{@game.penalties_away_country_goals})"
+      end
+    end
+  end
+
 end
