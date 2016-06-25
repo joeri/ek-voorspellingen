37ded6363198673eb2db5e854c64fecbdceb46a5
Joeri Samson <joerisamson@gmail.com>
2016-06-26 01:44:14 +0200

    Whoops

--- phoenix/web/views/game_view.ex
+++ phoenix/web/views/game_view.ex
@@ -9,13 +9,13 @@ defmodule EcPredictions.GameView do
   end
 
   def format_outcome(game) do
-    if @game.round == 1 do
-      "#{@game.home_country_goals}-#{@game.away_country_goals}"
+    if game.round == 1 do
+      "#{game.home_country_goals}-#{game.away_country_goals}"
     else
-      if @game.final_home_country_goals == @game.final_away_country_goals do
-        "#{@game.final_home_country_goals}-#{@game.final_away_country_goals}"
+      if game.final_home_country_goals == game.final_away_country_goals do
+        "#{game.final_home_country_goals}-#{game.final_away_country_goals} (#{game.penalties_home_country_goals}-#{game.penalties_away_country_goals})"
       else
-        "#{@game.final_home_country_goals}-#{@game.final_away_country_goals} (#{@game.penalties_home_country_goals}-#{@game.penalties_away_country_goals})"
+        "#{game.final_home_country_goals}-#{game.final_away_country_goals}"
       end
     end
   end
