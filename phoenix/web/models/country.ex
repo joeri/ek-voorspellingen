553862510d744eee1c55e4b3ff64ff2ce2b9b700
Joeri Samson <joerisamson@gmail.com>
2016-06-26 17:03:37 +0200

    A win is a win (for favourites)

--- phoenix/web/models/country.ex
+++ phoenix/web/models/country.ex
@@ -15,10 +15,10 @@ defmodule EcPredictions.Country do
 
   def points(home_games, away_games) do
     home_points = home_games
-    |> Enum.map(fn game -> score_for(game.home_country_goals, game.away_country_goals) end)
+    |> Enum.map(fn game -> {home,away} = scores_for(game); home end)
     |> Enum.sum
     away_points = away_games
-    |> Enum.map(fn game -> score_for(game.away_country_goals, game.home_country_goals) end)
+    |> Enum.map(fn game -> {home,away} = scores_for(game); away end)
     |> Enum.sum
     home_points + away_points
   end
@@ -32,12 +32,27 @@ defmodule EcPredictions.Country do
     |> validate_required([:name])
   end
 
-  defp score_for(self_goals, other_goals) do
+  defp scores_for(game) do
+    home_goals = game.home_country_goals
+    away_goals = game.away_country_goals
+
     cond do
-      self_goals == nil         -> 0
-      self_goals > other_goals  -> 5 + self_goals
-      self_goals == other_goals -> 3 + self_goals
-      self_goals < other_goals  -> 1 + self_goals
+      home_goals == nil         -> {0, 0}
+      home_goals > away_goals  -> {5 + home_goals, 1 + away_goals}
+      home_goals == away_goals ->
+        if game.round == 1 do
+          {3 + home_goals, 3 + away_goals}
+        else
+          home_score = [game.final_home_country_goals, game.penalties_home_country_goals]
+          away_score = [game.final_away_country_goals, game.penalties_away_country_goals]
+
+          if home_score > away_score do
+            {5 + home_goals, 1 + away_goals}
+          else
+            {1 + home_goals, 5 + away_goals}
+          end
+        end
+      home_goals < away_goals  -> {1 + home_goals, 5 + away_goals}
     end
   end
 end
